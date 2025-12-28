76d6cdf43c1cb5935e5bc7376930485284809ad2
Shaunak Basu <55021457+basushaunak@users.noreply.github.com>
2025-12-28 11:08:43 +0530

    Repo Init

---
+++ src/config.ts
@@ -0,0 +1,18 @@
+export const DB_NAME = "countryDB";
+export const STORE_NAME = "countries";
+export const DB_VERSION = 1.0;
+
+
+export interface Country  {
+    "name": string,
+    "capital": string,
+    "population": number,
+    "independenceOrFormation": string,
+    "formerColonialPower": string,
+    "isd_code": string,
+    "cctld": string,
+    "countryDescription": string,
+    "countryFlag": "🇩🇿",
+    "countryImage": Blob | null,
+    "continent": string
+  }
