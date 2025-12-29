6ff0907571efe96180695302b3a8961e5af271ae
Shaunak Basu <55021457+basushaunak@users.noreply.github.com>
2025-12-29 07:46:19 +0530

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,37 @@ import "./App.css";
 import type { Country } from "./config";
 import Countries from "./components/Countries";
 import allCountries from "./countries.json";
-// import { fetchCountrySummary } from "./utils";
+import { fetchCountrySummary } from "./utils";
+import { fetchCountrySummary } from "./utils";
 
 function App() {
   const [countries, setCountries] = useState(allCountries);
   // fetchCountrySummary(countries[0].name).then(res=>console.log(res.extract));
+  useEffect(() => {
+    async function populateDescriptions() {
+      const updatedCountries = await Promise.all(
+        countries.map(async (cntry) => {
+          if (cntry.countryDescription) {
+            return cntry;
+          } else {
+            const res = await fetchCountrySummary(cntry.name);
+            return {
+              ...cntry,
+              countryDescription: res.extract,
+            };
+          }
+        })
+      );
+
+      setCountries(updatedCountries);
+    }
+
+    populateDescriptions();
+  }, []);
+
   return (
     <>
-    <Countries countries={countries}/>
+      <Countries countries={countries} />
     </>
   );
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,37 @@
+import type { Country } from "./config";
+import { DB_NAME, STORE_NAME, DB_VERSION } from "./config";
+export function getCountries(): Country[] {
+  const request = indexedDB.open(DB_NAME, DB_VERSION);
+
+  request.onerror = (event) => {
+    console.error("An error occurred with IndexedDB");
+    console.error(event);
+  };
+  request.onupgradeneeded = () => {
+    const db = request.result;
+    const store = db.createObjectStore(STORE_NAME, { keyPath: "name" });
+    store.createIndex("continent", ["continent"], { unique: false });
+  };
+  request.onsuccess = () => {
+    const db = request.result;
+    const transaction = db.transaction(STORE_NAME, "readwrite");
+    const store = transaction.objectStore(STORE_NAME);
+    let temp = store.getAll();
+    let countries: Country[];
+    temp.onsuccess = () => {
+      countries = temp.result;
+      if (countries) {
+        return countries;
+      }
+    };
+  };
+}
+
+async function readCountries() {
+  let result = await fetch("/data/countries.json");
+  let ctry = await result.json();
+  return ctry;
+}
 export async function fetchCountrySummary(countryName: string) {
   const url = `https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(
     countryName
