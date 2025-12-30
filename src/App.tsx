f01854d3cec36a1c8ca9f63928ea40c6b0ea4f85
Shaunak Basu <55021457+basushaunak@users.noreply.github.com>
2025-12-30 16:53:34 +0530

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,20 @@ import { getCountries} from "./utils";
 
 function App() {
   const [countries, setCountries] = useState<Country[]>([]);
+  const [continent, setContinent]=useState("observer");
   // fetchCountrySummary(countries[0].name).then(res=>console.log(res.extract));
+  function changeContinent(cont:string):void{
+    setContinent(cont);
+  }
   useEffect(()=>{
     (async ()=>{
-      const cntryAll = await getCountries();
+      const cntryAll = await getCountries(continent);
       setCountries(cntryAll);
     })()
-  },[])
+  },[continent])
   return (
     <>
-      <Filter />
+      <Filter onChange={changeContinent} value={continent}/>
       <Countries countries={countries} />
     </>
   );
--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,16 +1,23 @@
-export default function Filter() {
+import "../styles/Filter.css";
+interface FilterProps{
+  onChange: (cont:string)=>void,
+  value:string,
+}
+export default function Filter({onChange,value}:FilterProps) {
   return (
     <div className="filter-box">
       <p>Select Continent</p>
-      <select>
+      <select id="filter" onChange={(e)=>onChange(e.target.value)} value={value}>
         <option value="africa">Africa</option>
         <option value="asia">Asia</option>
         <option value="europe">Europe</option>
-        <option value="north-america">North America</option>
-        <option value="south-america">South America</option>
-        <option value="oceania">Oceanea</option>
+        <option value="northamerica">North America</option>
+        <option value="southamerica">South America</option>
+        <option value="observer">Observer</option>
+        <option value="oceania">Oceania</option>
+        <option value="all">All</option>
       </select>
-      <button>Go!</button>
+      {/* <button>Go!</button> */}
     </div>
   );
 }
